8533cc22c4915bb128486f332b723a8458411af0
Alberto <arubalcaba@24hourfit.com>
2017-12-01 18:59:13 -0800

    Removed useless classes

--- src/main/java/com/shakepoint/web/io/data/dto/req/socket/MachineMessageType.java
+++ src/main/java/com/shakepoint/web/io/data/dto/req/socket/MachineMessageType.java
@@ -3,6 +3,7 @@ package com.shakepoint.web.io.data.dto.req.socket;
 public enum MachineMessageType {
     QR_CODE_DISPENSED("qr_code_dispensed"),
     TURN_ON("turned_on"),
+    QR_CODE_EXCHANGE("qr_code_exchange"),
     NOT_VALID("");
 
     String value;
@@ -13,6 +14,8 @@ public enum MachineMessageType {
     public static MachineMessageType get(String value){
         if (value.equals(TURN_ON.value)){
             return TURN_ON;
+        } else if (value.equals(QR_CODE_EXCHANGE.value)){
+            return QR_CODE_EXCHANGE;
         }else return NOT_VALID;
     }
 }
